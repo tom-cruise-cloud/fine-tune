bb7e04193e3534bbb5da2f69bffb737d7d48f444
tom-cruise-cloud <71536631+tom-cruise-cloud@users.noreply.github.com>
2025-08-19 15:01:49 -0400

    Create train_qwen3.py

---
+++ train_qwen3.py
@@ -0,0 +1,60 @@
+# Load model directly
+from datasets import Dataset
+from transformers import AutoTokenizer
+from transformers import Trainer
+from transformers import TrainingArguments
+from transformers import AutoModelForCausalLM
+
+data = [
+    {
+        "sentence1": "2 123456789010 eni-1235b8ca123456789 172.31.16.139 172.31.16.21 20641 22 6 20 4249 1418530010 1418530070 ACCEPT OK",
+        "sentence2": "In this example, SSH traffic (destination port 22, TCP protocol) from IP address 172.31.16.139 to network interface with private IP address is 172.31.16.21 and ID eni-1235b8ca123456789 in account 123456789010 was allowed.",
+        "label": 1
+    },
+    {
+        "sentence1": "2 123456789010 eni-1235b8ca123456789 172.31.9.69 172.31.9.12 49761 3389 6 20 4249 1418530010 1418530070 REJECT OK",
+        "sentence2": "In this example, RDP traffic (destination port 3389, TCP protocol) to network interface eni-1235b8ca123456789 in account 123456789010 was rejected.",
+        "label": 1
+    },
+        {
+        "sentence1": "2 123456789010 eni-1235b8ca123456789 172.31.16.139 172.31.16.21 20641 22 6 20 4249 1418530010 1418530070 ACCEPT OK",
+        "sentence2": "In this example, RDP traffic (destination port 3389, TCP protocol) to network interface eni-1235b8ca123456789 in account 123456789010 was rejected.",
+        "label": 0
+    },
+]
+
+dataset = Dataset.from_list(data)
+
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-4B")
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-4B")
+
+def tokenize_function(examples):
+    return tokenizer(
+            examples["sentence1"],
+            examples["sentence2"],
+            truncation=True,
+            padding="max_length", # or "longest"
+            max_length=128 # specify a max length if using "max_length"
+)
+
+tokenized_dataset = dataset.map(tokenize_function, batched=True)
+tokenized_dataset = tokenized_dataset.add_column("labels", dataset["label"])
+# tokenized_dataset = tokenized_dataset.rename_column("label", "labels")
+tokenized_dataset.set_format("torch", columns=["input_ids", "attention_mask", "labels"])
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    learning_rate=2e-5,
+    per_device_train_batch_size=8,
+    num_train_epochs=5,
+    save_steps=5
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset,
+    tokenizer=tokenizer
+)
+
+trainer.train()
