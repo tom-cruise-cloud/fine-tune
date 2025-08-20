0581e43f5c64b7c47972afed4b3bf60bc79e219e
tom-cruise-cloud <71536631+tom-cruise-cloud@users.noreply.github.com>
2025-08-20 16:29:25 -0400

    Update gpt_test.py

--- gpt_test.py
+++ gpt_test.py
@@ -1,11 +1,21 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_path = "./checkpoint-12/" # Replace with your model's local path
+model_path = "./results/checkpoint-40/" # Replace with your model's local path
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForCausalLM.from_pretrained(model_path)
 
-text_prompt = "2 123456789010 eni-1235b8ca123456789 172.31.16.139 172.31.16.21 20641 22 6 20 4249 1418530010 1418530070 ACCEPT OK"
+text_prompt = "2 123456789010 eni-1235b8ca123456789 172.31.9.69 172.31.9.12 49761 3389 6 20 4249 1418530010 1418530070 REJECT OK"
 inputs = tokenizer(text_prompt, return_tensors="pt")
-generated_ids = model.generate(inputs["input_ids"], max_new_tokens=150, num_beams=2, early_stopping=True, pad_token_id=tokenizer.eos_token_id,attention_mask=inputs['attention_mask'],)
+generated_ids = model.generate(
+    inputs["input_ids"], 
+    max_new_tokens=150, 
+    num_beams=3, 
+    pad_token_id=tokenizer.eos_token_id, 
+    attention_mask=inputs['attention_mask'],
+    do_sample=True,
+    temperature=0.8,
+    top_k=50,
+    top_p=0.95
+)
 generated_description = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
 print(generated_description)
