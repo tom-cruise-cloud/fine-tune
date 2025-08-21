809b127ef3070e26fbc6f1397f23a2e4e271f2ae
tom-cruise-cloud <71536631+tom-cruise-cloud@users.noreply.github.com>
2025-08-21 11:42:40 -0400

    Create instruct_training.py

---
+++ instruct_training.py
@@ -0,0 +1,37 @@
+from datasets import load_dataset
+from trl import SFTTrainer
+from datasets import Dataset
+from transformers import AutoTokenizer
+from peft import LoraConfig, get_peft_model
+from transformers import AutoModelForCausalLM, TrainingArguments, Trainer
+
+# Load your dataset
+
+data = [
+  {"instruction": "2 123456789010 eni-1235b8ca123456789 172.31.16.139 172.31.16.21 20641 22 6 20 4249 1418530010 1418530070 ACCEPT OK", "response": "SSH traffic (destination port 22, TCP protocol) from IP address 172.31.16.139 to network interface with private IP address is 172.31.16.21 and ID eni-1235b8ca123456789 in account 123456789010 was allowed."},
+  {"instruction": "2 123456789010 eni-1235b8ca123456789 172.31.9.69 172.31.9.12 49761 3389 6 20 4249 1418530010 1418530070 REJECT OK", "response": "RDP traffic (destination port 3389, TCP protocol) to network interface eni-1235b8ca123456789 in account 123456789010 was rejected."}
+]
+
+dataset = Dataset.from_list(data)
+# print(dataset)
+
+# model_name='meta-llama/Llama-2-7b-hf'
+model_name='Qwen/Qwen2-7B-Instruct'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    # torch_dtype="auto",
+    # device_map="auto"
+)
+
+# lora_config = LoraConfig(r=8, lora_alpha=16, target_modules=["q_proj", "v_proj"])
+# model = get_peft_model(model, lora_config)
+
+training_args = TrainingArguments(output_dir="./results", num_train_epochs=3)
+trainer = SFTTrainer(
+    model=model,
+    processing_class=tokenizer,
+    train_dataset=dataset,
+    args=training_args,
+    formatting_func=lambda x: f"### Instruction:\n{x['instruction']}\n### Output:\n{x['response']}",
+)
