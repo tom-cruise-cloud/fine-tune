4d69c722df186d02fc89585b76eb71c852f510c0
tom-cruise-cloud <71536631+tom-cruise-cloud@users.noreply.github.com>
2025-08-28 14:19:41 -0400

    Create test.py

---
+++ Qwen2.5-0.5B-Instruct/test.py
@@ -0,0 +1,41 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+model_name='./fine_tuned_model/'
+# model_name='./results/checkpoint-5'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+
+prompt = "2 123456789010 eni-1235b8ca123456789 172.31.9.69 172.31.9.12 49761 3389 6 20 4249 1418530010 1418530070 REJECT OK"
+messages = [
+    {"role": "user", "content": prompt}
+]
+
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt")
+# print(model_inputs.input_ids)
+
+# generated_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+# print(generated_text)
+
+generated_ids = model.generate(
+    model_inputs.input_ids,
+    attention_mask=model_inputs['attention_mask'],
+    max_new_tokens=512
+)
+# print(generated_ids)
+
+# generated_ids = [
+#     output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+# ]
+# print(generated_ids)
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print("response: " + response)
