736d2cac087108acdbf2c69e6bbc435e41bde07c
tom-cruise-cloud <71536631+tom-cruise-cloud@users.noreply.github.com>
2025-08-29 15:34:57 -0400

    Update falcon.py

--- falcon.py
+++ falcon.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import transformers
 import torch
 
-model_name = "tiiuae/falcon-7b"
+model_name = "tiiuae/Falcon3-3B-Instruct"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
