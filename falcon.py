3fba44ab724495290999003c1f12dd6afbbe8b8c
tom-cruise-cloud <71536631+tom-cruise-cloud@users.noreply.github.com>
2025-08-29 15:36:00 -0400

    Delete falcon.py

--- falcon.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import transformers
-import torch
-
-model_name = "tiiuae/Falcon3-3B-Instruct"
-
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
