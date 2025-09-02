f03d95c44d70eb94fbde2349d2d72d5eefdb2879
tom-cruise-cloud <71536631+tom-cruise-cloud@users.noreply.github.com>
2025-09-02 16:13:34 -0400

    Update train-s3.py

--- Qwen2.5-0.5B-Instruct/train-s3.py
+++ Qwen2.5-0.5B-Instruct/train-s3.py
@@ -23,16 +23,16 @@ data = [
 s3 = boto3.client('s3')
 bucket_name = 'ml-finetune'
 s3_key = 'model.tar.gz'
-local_path = 'model.tar'
+local_path = 'model.tar.gz'
 
 s3.download_file(bucket_name, s3_key, local_path)
 
 local_model_directory = "./local_model_directory"
 
-with tarfile.open(local_path, 'r') as tar:
-    tar.extractall('./local_model_directory')
+with tarfile.open(local_path, 'r:gz') as tar:
+    tar.extractall(local_model_directory)
 
-model_dir = './local_model_directory'
+model_dir = local_model_directory
 tokenizer = AutoTokenizer.from_pretrained(model_dir)
 model = AutoModelForCausalLM.from_pretrained(model_dir)
 
