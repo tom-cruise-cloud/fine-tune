d7e12c310beeab60ec89457471fdd2dfdfa7ff7f
tom-cruise-cloud <71536631+tom-cruise-cloud@users.noreply.github.com>
2025-09-29 11:40:35 -0400

    Update inference.py

--- falcon/inference.py
+++ falcon/inference.py
@@ -1,38 +1,41 @@
-import os
-import json
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
-def model_fn(model_dir):
-    """
-    Loads the pre-trained model from the model_dir.
-    """
-    # Assuming your model is a Hugging Face model stored in model_dir
-    model = pipeline("text-generation", model=model_dir)
-    return model
+model_name='./fine_tuned_model_v1/'
+# model_nmae = "tiiuae/Falcon3-10B-Instruct"
+# model_name='./results/checkpoint-5'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    # device_map="auto"
+)
 
-def input_fn(request_body, content_type):
-    """
-    Deserializes the input data.
-    """
-    if content_type == 'application/json':
-        data = json.loads(request_body)
-        return data['inputs']
-    else:
-        raise ValueError(f"Unsupported content type: {content_type}")
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
 
-def predict_fn(input_object, model):
-    """
-    Performs inference using the loaded model.
-    """
-    # Generate text using the Hugging Face pipeline
-    generated_text = model(input_object, max_new_tokens=50)
-    return generated_text[0]['generated_text']
+prompt = "2 123456789010 eni-1235b8ca123456789 172.31.9.69 172.31.9.12 49761 3389 6 20 4249 1418530010 1418530070 REJECT OK"
 
-def output_fn(prediction, accept_type):
-    """
-    Serializes the prediction result.
-    """
-    if accept_type == 'application/json':
-        return json.dumps({'generated_text': prediction}), accept_type
-    else:
-        raise ValueError(f"Unsupported accept type: {accept_type}")
+messages = [
+    {"role": "user", "content": prompt}
+]
+
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt")
+
+generated_ids = model.generate(
+    model_inputs.input_ids,
+    attention_mask=model_inputs['attention_mask'],
+    pad_token_id=tokenizer.eos_token_id, 
+    max_new_tokens=1024,
+    do_sample=True,
+    top_k=50,
+    top_p=0.95,
+    temperature=0.85,
+)
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print("response: " + response)
