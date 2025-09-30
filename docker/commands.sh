2094c482dda3b4cf80ce5f4c7dbf953a26b541b7
tom-cruise-cloud <71536631+tom-cruise-cloud@users.noreply.github.com>
2025-09-30 11:10:45 -0400

    Update commands.sh

--- docker/commands.sh
+++ docker/commands.sh
@@ -32,4 +32,16 @@ falcon3-10b-inference                                                latest    1
     ]
 }
 
-docker push 837369895783.dkr.ecr.us-east-1.amazonaws.com/falcon3-10b-inference:latest
+sagemaker-user@default:~$ docker push 837369895783.dkr.ecr.us-east-1.amazonaws.com/falcon3-10b-inference:latest
+The push refers to repository [837369895783.dkr.ecr.us-east-1.amazonaws.com/falcon3-10b-inference]
+ecb610846dc8: Pushed 
+3addcb90c002: Pushed 
+bd831d67c76e: Pushed 
+1b60f954bada: Pushed 
+85e6d40487f0: Pushed 
+067ea27560c1: Pushed 
+7fb1037e08b3: Pushed 
+14cbeede8d6e: Pushed 
+ae2d55769c5e: Pushed 
+e2ef8a51359d: Pushed 
+latest: digest: sha256:2f142d6868f81ade85eabce64dcdcdc0e840c58e6db2373295fa11391fd3ed59 size: 2417
