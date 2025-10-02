f564022d33b43af1126264dfd5a3eba8f6807f3c
tom-cruise-cloud <71536631+tom-cruise-cloud@users.noreply.github.com>
2025-10-02 16:24:08 -0400

    Update wsgi.py

--- docker/wsgi.py
+++ docker/wsgi.py
@@ -1,4 +1,8 @@
 from inference import app as application
 
+app = application
 if __name__ == '__main__':
     application.run()
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080)
