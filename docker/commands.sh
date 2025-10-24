13f040f8ce7d9036c016ea451bfb85ff78056dbb
tom-cruise-cloud <71536631+tom-cruise-cloud@users.noreply.github.com>
2025-10-24 11:05:13 -0400

    Update commands.sh

--- docker/commands.sh
+++ docker/commands.sh
@@ -2,18 +2,18 @@ docker build --network sagemaker -t falcon3-10b-inference .
 
 docker run --network sagemaker falcon3-10b-inference
 
-sagemaker-user@default:~$ docker rmi 837369895783.dkr.ecr.region.amazonaws.com/falcon3-10b-inference:latest
+docker rmi 837369895783.dkr.ecr.region.amazonaws.com/falcon3-10b-inference:latest
 Untagged: 837369895783.dkr.ecr.region.amazonaws.com/falcon3-10b-inference:latest
 
-sagemaker-user@default:~$ aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 837369895783.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 837369895783.dkr.ecr.us-east-1.amazonaws.com
 WARNING! Your password will be stored unencrypted in /home/sagemaker-user/.docker/config.json.
 Configure a credential helper to remove this warning. See
 https://docs.docker.com/engine/reference/commandline/login/#credential-stores
 
 Login Succeeded
 
-sagemaker-user@default:~$ docker tag falcon3-10b-inference:latest 837369895783.dkr.ecr.us-east-1.amazonaws.com/falcon3-10b-inference:latest
-sagemaker-user@default:~$ docker images
+docker tag falcon3-10b-inference:latest 837369895783.dkr.ecr.us-east-1.amazonaws.com/falcon3-10b-inference:latest
+docker images
 REPOSITORY                                                           TAG       IMAGE ID       CREATED          SIZE
 837369895783.dkr.ecr.us-east-1.amazonaws.com/falcon3-10b-inference   latest    1086e125886f   35 minutes ago   7.4GB
 falcon3-10b-inference                                                latest    1086e125886f   35 minutes ago   7.4GB
