89f1f2a7739f43535c190850f437e193f2136a96
tom-cruise-cloud <71536631+tom-cruise-cloud@users.noreply.github.com>
2025-11-05 15:45:33 -0500

    Update commands.sh

--- docker/commands.sh
+++ docker/commands.sh
@@ -1,7 +1,8 @@
 aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query Vpc.VpcId --output text
-aws ec2 create-subnet --vpc-id <your-vpc-id> --cidr-block 10.0.1.0/24 --availability-zone us-east-1a --query Subnet.SubnetId --output text
-aws ec2 create-subnet --vpc-id <your-vpc-id> --cidr-block 10.0.2.0/24 --availability-zone us-east-1b --query Subnet.SubnetId --output text
-aws ec2 create-subnet --vpc-id <your-vpc-id> --cidr-block 10.0.3.0/24 --availability-zone us-east-1c --query Subnet.SubnetId --output text
+$vpcid =  
+aws ec2 create-subnet --vpc-id $vpcid --cidr-block 10.0.1.0/24 --availability-zone us-east-1a --query Subnet.SubnetId --output text
+aws ec2 create-subnet --vpc-id $vpcid --cidr-block 10.0.2.0/24 --availability-zone us-east-1b --query Subnet.SubnetId --output text
+aws ec2 create-subnet --vpc-id $vpcid --cidr-block 10.0.3.0/24 --availability-zone us-east-1c --query Subnet.SubnetId --output text
 
 
 docker build --network sagemaker -t falcon3-10b-inference .
